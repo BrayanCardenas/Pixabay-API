92ea649d211a05d554084312f9b69b76b95e5ce2
BrayanCardenas <brayansti34@gmail.com>
2023-01-12 17:51:36 -0500

    Rediseño con scss

--- js/app.js
+++ js/app.js
@@ -1,143 +1,166 @@
-
-const resultado = document.querySelector('#resultado');
-const paginacionDiv = document.querySelector('#paginacion');
+const resultado = document.querySelector("#resultado");
+const paginacionDiv = document.querySelector("#paginacion");
 
 let paginaActual = 1;
 let totalPaginas;
 let iteradorSiguiente;
 
 window.onload = () => {
-    const formulario = document.querySelector('#formulario');
-    formulario.addEventListener('submit', validarFormulario);
-    paginacionDiv.addEventListener('click', direccionPaginacion);
+  const formulario = document.querySelector("#formulario");
+  formulario.addEventListener("submit", validarFormulario);
+  paginacionDiv.addEventListener("click", direccionPaginacion);
 };
 
 function validarFormulario(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const terminoBusqueda = document.querySelector('#termino').value;
+  const terminoBusqueda = document.querySelector("#termino").value;
 
-    if(terminoBusqueda === '') {
-        // mensaje de error
-        mostrarAlerta('Agrega un término de búsqueda');
-        return;
-    }
+  if (terminoBusqueda === "") {
+    // mensaje de error
+    mostrarAlerta("Agrega un término de búsqueda");
+    return;
+  }
 
-    buscarImagenes();
+  buscarImagenes();
 }
-
 
 // Muestra una alerta de error o correcto
 function mostrarAlerta(mensaje) {
-    const alerta = document.querySelector('.bg-red-100');
-    if(!alerta) {
-        const alerta = document.createElement('p');
+  const alerta = document.querySelector(".bg-red-100");
+  if (!alerta) {
+    const alerta = document.createElement("p");
 
-        alerta.classList.add('bg-red-100', "border-red-400", "text-red-700", "px-4", "py-3", "rounded",  "max-w-lg", "mx-auto", "mt-6", "text-center" );
-    
-        alerta.innerHTML = `
+    alerta.classList.add(
+      "bg-red-100",
+      "border-red-400",
+      "text-red-700",
+      "px-4",
+      "py-3",
+      "rounded",
+      "max-w-lg",
+      "mx-auto",
+      "mt-6",
+      "text-center"
+    );
+
+    alerta.innerHTML = `
             <strong class="font-bold">Error!</strong>
             <span class="block sm:inline">${mensaje}</span>
         `;
-    
-        formulario.appendChild(alerta);
-    
-        setTimeout(() => {
-            alerta.remove();
-        }, 3000);
-    }
-}
 
+    formulario.appendChild(alerta);
+
+    setTimeout(() => {
+      alerta.remove();
+    }, 3000);
+  }
+}
 
 // Busca las imagenes en una API
 function buscarImagenes() {
-    const terminoBusqueda = document.querySelector('#termino').value;
+  const terminoBusqueda = document.querySelector("#termino").value;
 
-    const key = '1732750-d45b5378879d1e877cd1d35a6';
-    const url = `https://pixabay.com/api/?key=${key}&q=${terminoBusqueda}&per_page=30&page=${paginaActual}`;
+  const key = "1732750-d45b5378879d1e877cd1d35a6";
+  const url = `https://pixabay.com/api/?key=${key}&q=${terminoBusqueda}&per_page=30&page=${paginaActual}`;
 
-    fetch(url) 
-        .then(respuesta => respuesta.json())
-        .then( resultado => {
-            totalPaginas = calcularPaginas(resultado.totalHits);
-
-            // console.log(totalPaginas)
-
-            mostrarImagenes(resultado.hits);
-        });
-
-
+  fetch(url)
+    .then((respuesta) => respuesta.json())
+    .then((resultado) => {
+      totalPaginas = calcularPaginas(resultado.totalHits);
+      mostrarImagenes(resultado.hits);
+    });
 }
 
-function mostrarImagenes(imagenes, paginas ) {
+function mostrarImagenes(imagenes, paginas) {
+  while (resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild);
+  }
 
-    while(resultado.firstChild) {
-        resultado.removeChild(resultado.firstChild);
-    }
+  imagenes.map((imagen) => {
+    const {
+      likes,
+      views,
+      tags,
+      largeImageURL,
+      webformatURL,
+      comments,
+      downloads,
+    } = imagen;
+    resultado.innerHTML += `<div class="card">
+                              <img class="card__img" src=${webformatURL} height="300"  loading="lazy" alt=${tags}/>
+                              <div class="flex">
+                                <div>
+                                  <p class="card__p"><span class="card__span">${views} </span>Vistas</p>
+                                  <p class="card__p"><span class="card__span">${downloads} </span>Descargas</p>
+                                  <p class="card__p"><span class="card__span">${likes} </span>Me Gusta</p>
+                                </div>
+                                <div>
+                                  <p class="card__p"> <span class="card__span"> ${comments} </span>Comentarios</p>
+                                  <br/>
+                                  <p class="card__p"> <span class="card__span card__span--tags">tags </span>${tags}</p>
+                                </div>
+                              </div>
+                              <a
+                                  href="${largeImageURL}"
+                                  rel="noopener noreferrer"
+                                  target="_blank"
+                                  class="card__a"
+                                  >Ver Imagen
+                                </a>
+                            </div>`;
+  });
 
-    imagenes.forEach( imagen => {
-
-        const { likes, views, previewURL, largeImageURL } = imagen;
-        resultado.innerHTML += `
-            <div class="w-1/2 md:w-1/3 lg:w-1/4 mb-4 p-3">
-                <div class="bg-white ">
-                    <img class="w-full" src=${previewURL} alt={tags} />
-                    <div class="p-4">
-                        <p class="card-text">${likes} Me Gusta</p>
-                        <p class="card-text">${views} Vistas </p>
-        
-                        <a href=${largeImageURL} 
-                        rel="noopener noreferrer" 
-                        target="_blank" class="bg-blue-800 w-full p-1 block mt-5 rounded text-center font-bold uppercase hover:bg-blue-500 text-white">Ver Imagen</a>
-                    </div>
-                </div>
-            </div>
-            `;
-    });
-
-
-    if(!iteradorSiguiente) {
-        mostrarPaginacion();
-    }
- 
+  if (!iteradorSiguiente) {
+    mostrarPaginacion();
+  }
 }
 
 function mostrarPaginacion() {
-    // recorrer el iterador
-    iteradorSiguiente = crearPaginacion(totalPaginas);
-    while( true ) {
-        const { value, done } = iteradorSiguiente.next();
+  // recorrer el iterador
+  iteradorSiguiente = crearPaginacion(totalPaginas);
+  while (true) {
+    const { value, done } = iteradorSiguiente.next();
 
-        if(done) return;
+    if (done) return;
 
-        // Crear botón de sig
-        const botonSiguiente = document.createElement('a');
-        botonSiguiente.href = "#";
-        botonSiguiente.dataset.pagina = value;
-        botonSiguiente.textContent = value;
-        botonSiguiente.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'mx-auto', 'mb-10', 'font-bold', 'uppercase', 'rounded');
-        paginacionDiv.appendChild(botonSiguiente);
-    }
+    // Crear botón de sig
+    const botonSiguiente = document.createElement("a");
+    botonSiguiente.href = "#";
+    botonSiguiente.dataset.pagina = value;
+    botonSiguiente.textContent = value;
+    botonSiguiente.classList.add(
+      "siguiente",
+      "bg-yellow-400",
+      "px-4",
+      "py-1",
+      "mr-2",
+      "mx-auto",
+      "mb-10",
+      "font-bold",
+      "uppercase",
+      "rounded"
+    );
+    paginacionDiv.appendChild(botonSiguiente);
+  }
 }
 
 function calcularPaginas(total) {
-    return parseInt( Math.ceil( total / 30 ));
+  return parseInt(Math.ceil(total / 30));
 }
 
-
 // Crear el generador
-function *crearPaginacion(total) {
-    console.log(total);
-    for( let i = 1; i <= total; i++) {
-        yield i;
-    }
+function* crearPaginacion(total) {
+  console.log(total);
+  for (let i = 1; i <= total; i++) {
+    yield i;
+  }
 }
 
 function direccionPaginacion(e) {
-    if(e.target.classList.contains('siguiente')) {
-
-        paginaActual= Number( e.target.dataset.pagina);
-        buscarImagenes();
-        formulario.scrollIntoView();
-    }
+  if (e.target.classList.contains("siguiente")) {
+    paginaActual = Number(e.target.dataset.pagina);
+    buscarImagenes();
+    formulario.scrollIntoView();
+  }
 }
